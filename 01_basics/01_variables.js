1f92f9dbf9afa09d0e4c75ec9405472df73a88b4
Khushter Adeeb <115522888+Khushteradeeb@users.noreply.github.com>
2024-04-25 14:40:12 +0000

    variable and constant declaration

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,14 +1,22 @@
-const acountId = 12344
-let emailId = adeeb@123
-var emailPassword = 12345678
-var acountCity = muzaffarpur
+const accountId = 144553
+let accountEmail = "adeeb@google.com"
+var accountPassword = "12345"
+accountCity = "Muzaffarpur"
+let accountState;
+
+// accountId = 2 // not allowed
 
 
-acountId = 222222     // Not Allowed to change the constant - const in javascript 
+accountEmail = "ad@eb.com"
+accountPassword = "55552121"
+accountCity = "Delhi"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
 
 
-emailId = khushter@22
-emailPassword = 222222
-acountCity = delhi
-
-console.log(acountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
