34811b5eeb02020b83ffad3afbcccc848e738eb3
Khushter Adeeb <115522888+Khushteradeeb@users.noreply.github.com>
2024-04-25 16:18:18 +0000

    Conversion of Datatypes

---
+++ 01_basics/03_conversionsOperation.js
@@ -0,0 +1,28 @@
+let score = "adeeb"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "adeeb"
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanisLoggedIn);
+
+ // 1 => true; 0 => false
+ // "" => false
+ // "adeeb" => true
+
+ let someNumber = 33
+ 
+ let stringNumber = String(someNumber)
+ console.log(stringNumber);
+ console.log(typeof stringNumber);
