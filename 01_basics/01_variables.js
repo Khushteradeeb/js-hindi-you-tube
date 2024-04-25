f412d3b16bfd4e501ac6a9704f641601bf62f076
Khushter Adeeb <115522888+Khushteradeeb@users.noreply.github.com>
2024-04-25 09:06:00 +0000

    const and variable

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const acountId = 12344
+let emailId = adeeb@123
+var emailPassword = 12345678
+var acountCity = muzaffarpur
+
+
+acountId = 222222     // Not Allowed to change the constant - const in javascript 
+
+
+emailId = khushter@22
+emailPassword = 222222
+acountCity = delhi
+
+console.log(acountId);
