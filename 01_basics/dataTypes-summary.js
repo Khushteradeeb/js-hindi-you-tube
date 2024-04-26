44eb92f5fd6839754c4570e76f4953375e0b4be9
Khushter Adeeb <115522888+Khushteradeeb@users.noreply.github.com>
2024-04-26 09:58:11 +0000

    Learnt about Stack & Heap Memory

--- 01_basics/dataTypes-summary.js
+++ 01_basics/dataTypes-summary.js
@@ -26,18 +26,43 @@ let myObj = {
     age: 22,
 }
 
- const myFunction = function(){
-     console.log("Hello World");
+//  const myFunction = function(){
+//      console.log("Hello World");
 
- }
+//  }
 
  // typeOf checking
 
- console.log(typeof outsideTemp);
- console.log(typeof scoreValue);
- console.log(typeof BigNumber);
- console.log(typeof myFunction);
+//  console.log(typeof outsideTemp);
+//  console.log(typeof scoreValue);
+//  console.log(typeof BigNumber);
+//  console.log(typeof myFunction);
 
 
 //    Read the Document from Original ECMA Script
  // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+ // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+ // Stack(Primitive) , Heap(Non Primitive)
+
+ let myYouTubeName = "Khushter Adeeb"
+
+ let anotherName = myYouTubeName
+ anotherName = "Science"
+
+ console.log(anotherName);
+ console.log(myYouTubeName);
+
+ let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+ }
+
+ let userTwo = userOne
+ userTwo.email = "hitesh@google.com"
+
+ console.log(userOne.email);
+console.log(userTwo.email);
