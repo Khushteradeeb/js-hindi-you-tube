88b8ae7b8d5aec36481b1a353ceb6a1121a81e46
Khushter Adeeb <115522888+Khushteradeeb@users.noreply.github.com>
2024-04-26 15:08:01 +0000

    understood the date,to_values

---
+++ 01_basics/07_datesInJS.js
@@ -0,0 +1,24 @@
+ // DATES 
+
+ let myDate = new Date()
+ //console.log(myDate.toString());  //output: Fri Apr 26 2024 14:50:55 GMT+0000 (Coordinated Universal Time)
+
+// console.log(myDate.toDateString());    // output:[Fri Apr 26 2024]
+
+ //console.log(myDate.toLocaleDateString()); // O/P : 4/26/2024
+
+ //console.log(myDate.toISOString());  // O/P : 2024-04-26T15:03:51.260Z
+
+ //console.log(myDate.toJSON());  // O/P : 2024-04-26T15:03:51.260Z
+
+
+ //console.log(myDate.toLocaleString()); // O/P : 4/26/2024, 3:03:51 PM
+
+
+ //console.log(myDate.toDateString());  // O/P : Fri Apr 26 2024
+
+
+ //console.log(myDate.toLocaleTimeString());  // o/P : 3:03:51 PM
+
+
+console.log(typeof myDate);
